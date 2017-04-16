d7d4193b77f73059b41c44f35348133419685f3d
formula <formula22_1@yahoo.co.jp>
2017-04-16 14:52:02 +0900

    [bookmarklet] URL指定ダウンロードのパラメタにリファラー追加

--- download/common/get_urldlcontents.php
+++ download/common/get_urldlcontents.php
@@ -1,12 +1,14 @@
 <?php
-function get_urldlcontents($url, $file_name){
+function get_urldlcontents($url, $file_name, $referer=null){
     #header
     $headers = get_headers($url, 1);
     if ($headers['Content-Type'] != null) {
         header("Content-Type: ".$headers['Content-Type']."\r\n");
     }
     header("Access-Control-Allow-Origin: *\r\n");
-    header("Referer: ".$url."\r\n");
+    if ($referer != null) {
+        header("Referer: ".$referer."\r\n");
+    }
     header("Content-Disposition: attachment; filename=\"".$file_name."\"\r\n");
 
     #contents
--- download/common/url_download.php
+++ download/common/url_download.php
@@ -6,7 +6,7 @@ function put_help() {
     <html><head><title>help</title></head>
     <body>
         [how to use]<br>
-        /url_download.php?url=[ダウンロードしたいURL]&dlname=[ダウンロードコンテンツにつけるファイル名]
+        /url_download.php?url=[ダウンロードしたいURL]&dlname=[ダウンロードコンテンツにつけるファイル名]&referer=[リファラー]
     </body>
     </html>
     ");
@@ -19,8 +19,12 @@ if ($url == null) {
     if ( $dlname == null) {
         $dlname = 'tmp';
     }
+    $referer = $_GET['referer'];
+    if ( $referer == null) {
+        $referer = $url;
+    }
     require_once('get_urldlcontents.php');
-    $file = get_urldlcontents($url, $dlname);
+    $file = get_urldlcontents($url, $dlname, $referer);
     echo $file;
 }
 ?>
